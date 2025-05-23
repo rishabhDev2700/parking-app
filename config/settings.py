4ae555deb4af00aedfab386fb81579e854b653d5
rishabhdev2700 <rishavkumar2700@gmail.com>
2025-05-23 12:01:23 +0100

    Added allowed host

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG", bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
